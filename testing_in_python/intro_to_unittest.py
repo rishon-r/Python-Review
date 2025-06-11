3e2f186746805784ce1eda5c4523dfedcf9140c6
Rishon <rishonrajesh13@gmail.com>
2025-06-11 12:54:25 -0700

    Folder for studying testing in Python created

---
+++ testing_in_python/intro_to_unittest.py
@@ -0,0 +1,5 @@
+# Testing is a very important part of writing code
+# Python allows us to write tests for our code using the unittest module
+# You can access said module by importing it
+import unittest
+
