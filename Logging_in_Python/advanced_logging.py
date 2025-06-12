21af565627cc41c0866b5e678c51346fa72db335
Rishon <rishonrajesh13@gmail.com>
2025-06-12 00:26:58 -0700

    Finalized changes to logging notes

--- Logging_in_Python/advanced_logging.py
+++ Logging_in_Python/advanced_logging.py
@@ -15,6 +15,15 @@ logger.setLevel(logging.INFO)
 # This handler is responsible for directing the log output to a file.
 file_handler = logging.FileHandler('sample.log')
 
+# A StreamHandler sends log messages to a stream — by default, this is sys.stderr (the console).
+# This is useful for immediate feedback during development or for logging runtime events in real time.
+# Unlike the FileHandler (which persists logs), the StreamHandler simply displays them on-screen.
+stream_handler = logging.StreamHandler()
+
+# We can set level for stream handler and file handler as well separately
+# This will allow us to choose the security level of messages that go into the file and directly to console respectively
+stream_handler.setLevel(logging.DEBUG) # Allowing stream handler to handle debug messages
+
 # Define a formatter that specifies the log message format.
 # Here, each log will include:
 # - the current timestamp (asctime)
@@ -22,13 +31,21 @@ file_handler = logging.FileHandler('sample.log')
 # - the actual log message (message)
 formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
 
+# Define a simpler format for console output
+stream_formatter = logging.Formatter("%(levelname)s: %(message)s")
+
 # Attach the formatter to the file handler so that the log messages are written in the specified format.
 file_handler.setFormatter(formatter)
+
+# Attach the formatter to the stream handler
+stream_handler.setFormatter(stream_formatter)
 
 # Add the file handler to the logger.
 # This step "plugs in" the handler so that the logger knows where and how to output log messages.
 logger.addHandler(file_handler)
+logger.addHandler(stream_handler)  # Show logs on console
 
 # Log a message at INFO level.
 # Since we've set the logger's level to INFO, this message will be written to 'sample.log'.
+# It will also be printed to console
 logger.info("Logger object Created")
